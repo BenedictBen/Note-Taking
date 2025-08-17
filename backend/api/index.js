5b701e4df35e70c35a04b758e57eb28f3838ad3c
BenedictBen <benbenedict26@yahoo.com>
2025-08-17 17:31:16 +0800

    Fixing Deployemt error

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,31 +1,4 @@
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const cors = require('cors');
-// require('dotenv').config();
 
-// const app = express();
-
-// // Middleware
-// app.use(cors());  // Allows frontend to connect
-// app.use(express.json());  
-
-// // Database connection
-// mongoose.connect(process.env.MONGODB_URI)
-//   .then(() => console.log("Connected to MongoDB"))
-//   .catch(err => console.error("MongoDB connection error:", err));
-
-// // Basic route to test server
-// app.get('/', (req, res) => {
-//   res.send('Note-Taking API is running!');
-// });
-
-// const notesRouter = require('./routes/notes');
-// app.use('/api/notes', notesRouter);
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
 
 const express = require('express');
 const mongoose = require('mongoose');
